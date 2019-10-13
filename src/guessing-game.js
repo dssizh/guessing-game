781a434351baccd2752b3fb4ff43ad7559d5aee6
Dmitriy S <sutyginds@gmail.com>
2019-10-13 10:57:37 +0400

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
+
     constructor() {}
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
 
+        if (this._min <= this._max) {
+            this.m = Math.round((this._min+this._max)/2);
+        } else {
+            return this._min;
+        }
+
+        return this.m;
     }
 
     lower() {
-
+        this._max = this.m;
     }
 
     greater() {
-
+       this._min = this.m;
+    //    this.min = this.m;
     }
 }
 
